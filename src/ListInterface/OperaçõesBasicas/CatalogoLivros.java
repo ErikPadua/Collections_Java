1fd9ca5ad48958636d024d5bf7facfca8b6338a6
Erik Pádua <erikpadua76@gmail.com>
2024-05-14 21:20:55 -0300

    0.2

---
+++ src/ListInterface/OperaçõesBasicas/CatalogoLivros.java
@@ -0,0 +1,94 @@
+package ListInterface.OperaçõesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros 
+{//Testador
+    public static void main(String[] args) 
+    {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntevaloAnos(2020, 2022));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+    
+    //------------------------------------------------Código----------------------------------------------------------
+   
+    //Cria uma lista de livros
+    private List<Livro> listaDeLivros;
+
+    //Construtor que instância uma lista vazia
+    public CatalogoLivros() 
+    {
+        this.listaDeLivros = new ArrayList<>();
+    }
+
+    //Adiciona um livro a lista
+    public void adicionarLivro(String titulo,String autor,int ano)
+    {
+        listaDeLivros.add(new Livro(titulo, autor, ano));
+    }
+
+    //Seleciona um ou mais livros pelo autor,
+    //caso a lista estiver vazia o código não executara
+    public List<Livro> pesquisarPorAutor(String autor)
+    {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!listaDeLivros.isEmpty())
+        {
+            for(Livro l:listaDeLivros)
+            {
+                if(l.getAutor().equals(autor))
+                {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    //Seleciona um ou mais livro por um escopo de ano inicial e ano final, 
+    //caso a lista estiver vazia o código mão executara
+    public List<Livro> pesquisarPorIntevaloAnos(int anoInicial, int anoFinal)
+    {
+        List<Livro> livrosPorAno = new ArrayList<>();
+        if(!listaDeLivros.isEmpty())
+        {
+            for(Livro l:listaDeLivros)
+            {
+            if(l.getAno() >= anoInicial && l.getAno() <= anoFinal)
+                {
+                    livrosPorAno.add(l);
+                }
+            }
+        }
+        return livrosPorAno;
+    }
+
+    //Seleciona um livro pelo titulo, caso a lista estiver vazia o código mão executara
+    public Livro pesquisarPorTitulo(String titulo)
+    {
+        Livro livro = null;
+        if(!listaDeLivros.isEmpty())
+        {
+            for(Livro l:listaDeLivros)
+            {
+                if(l.getTitulo().equalsIgnoreCase(titulo))
+                {
+                    livro = l;
+                    break;
+                }
+            }
+        }
+        return livro;
+    }
+
+    
+}
---
+++ src/ListInterface/OperaçõesBasicas/Livro.java
@@ -0,0 +1,40 @@
+package ListInterface.OperaçõesBasicas;
+
+public class Livro 
+{
+    //Atributos
+    private String titulo;
+    private String autor;
+    private int ano;
+    
+    //Construtor
+    public Livro(String titulo, String autor, int ano) 
+    {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    //Retorna o titulo
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    //Retorna o autor
+    public String getAutor() {
+        return autor;
+    }
+
+    //Retorna o ano de publicação
+    public int getAno() {
+        return ano;
+    }
+
+    //Sobrescrevendo toString
+    @Override
+    public String toString() {
+        return "Livro [titulo=" + titulo + ", autor=" + autor + ", ano=" + ano + "]";
+    }
+
+  
+}
